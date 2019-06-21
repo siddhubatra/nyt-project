c1b9863d412a75b416b768fb4732012b096d59e8
thesidbatra <thesidbatra@gmail.com>
2019-06-21 13:49:52 -0400

    html changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,17 @@
-
-
-
 var search;
 var NumOfRecords;
 var startYear;
 var EndYear;
 
-var queryURL ="https://developer.nytimes.com/?t=" + search + "&apikey=1ZmOQO2w6gcCbNrBKImoAGRGW8fSVrnC"
 
 
-function search(){
+var queryURL = "https://developer.nytimes.com/?t=" + search + "&apikey=1ZmOQO2w6gcCbNrBKImoAGRGW8fSVrnC"
+
+function search() {
     $.ajax({
-    url: queryURL,
-    method: "GET"
-    }).then(function(response){
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
 
-});
+    });
 }
